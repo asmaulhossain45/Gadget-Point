bc207369e19d89af86f3fbd92cd3d66afa696cdf
asmaulhosen45 <asmaulhosen45@gmail.com>
2023-10-21 20:49:56 +0600

    navbar responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,6 +6,7 @@ import { AuthContext } from "../Routes/AuthProvider";
 const Navbar = () => {
   const { user, handleLogOut, setUser } = useContext(AuthContext);
   const [menuToggle, setMenuToggle] = useState(true);
+  const [profileToggle, setProfileToggle] = useState(true);
   const navigate = useNavigate();
 
   const handleLogOutButton = () => {
@@ -43,49 +44,37 @@ const Navbar = () => {
     </>
   );
 
+  console.log(user);
+
   return (
-    <div className="px-4 navbar bg-opacity-95 bg-slate-900 text-sky-400 font-semibold">
+    <div className="pr-4 md:px-6 navbar bg-slate-900 text-sky-400 font-semibold">
       <div className="navbar-start">
-        <div className="dropdown z-50">
-          <label
-            onClick={() => setMenuToggle(!menuToggle)}
-            tabIndex={0}
-            className="btn btn-ghost md:hidden"
-          >
-            {menuToggle ? (
-              <svg
-                className="swap-off fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
-                viewBox="0 0 512 512"
-              >
-                <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
-              </svg>
-            ) : (
-              <svg
-                className="swap-on fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
-                viewBox="0 0 512 512"
-              >
-                <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
-              </svg>
-            )}
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost md:hidden">
+            <svg
+              onClick={() => setMenuToggle(!menuToggle)}
+              className="swap-off fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 512 512"
+            >
+              <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+            </svg>
           </label>
           <ul
+            onClick={() => setMenuToggle(!menuToggle)}
             tabIndex={0}
             className={`${
-              menuToggle ? "hidden" : ""
-            } menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52`}
+              menuToggle ? "" : "hidden"
+            } menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-slate-900 rounded-box w-52`}
           >
             {NavLinks}
           </ul>
         </div>
         <Link to="/">
           <img
-            className="sm:ml-2 md:ml-0 w-28"
+            className="w-28"
             src="https://i.ibb.co/DD31dTM/Logo.png"
             alt=""
           />
@@ -96,10 +85,11 @@ const Navbar = () => {
       </div>
       <div className="navbar-end">
         {user ? (
-          <div className="dropdown dropdown-end z-50">
+          <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
                 <img
+                  onClick={() => setProfileToggle(!profileToggle)}
                   src={
                     user.photoURL
                       ? user.photoURL
@@ -110,23 +100,25 @@ const Navbar = () => {
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              className={`${
+                profileToggle ? "" : "hidden"
+              } menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-slate-900 rounded-box w-52 flex justify-center`}
             >
-              <li>
-                <p>{user.displayName}</p>
-              </li>
-              <li>
-                <p>{user.email}</p>
-              </li>
-              <li>
-                <button onClick={handleLogOutButton}>Log Out</button>
-              </li>
+              {user.emailVerified ? (
+                <h1 className="text-xs border-2 border-green-600 py-1 rounded-full bg-white text-green-600 flex justify-center">
+                  VERIFIED
+                </h1>
+              ) : (
+                <h1 className="text-xs border-2 border-red-600 py-1 rounded-full bg-white text-red-600 flex justify-center">UNVERIFIED</h1>
+              )}
+
+              <p className="text-center mt-1">{user.displayName}</p>
+              <button className="mt-1" onClick={handleLogOutButton}>Log Out</button>
             </ul>
           </div>
         ) : (
-          <button>
-            <NavLink to="/login">Login</NavLink>
-          </button>
+          <Link to="/login"
+          className="btn btn-ghost">Login</Link>
         )}
       </div>
     </div>
